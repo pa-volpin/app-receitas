8a0dc8e0b39cbdc6815710e8d9864cfdc7f70015
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-11-19 17:22:24 -0300

    :sparkles: Check de email e senha
    Introduzimos Regex e verificacão de email e senha

--- src/context/ReceitasProvider.jsx
+++ src/context/ReceitasProvider.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import propTypes from 'prop-types';
 import ReceitasContext from './ReceitasContext';
 
 function ReceitasProvider({ children }) {
+  const [email, setEmail] = useState('');
+
   return (
-    <ReceitasContext.Provider value={{}}>
+    <ReceitasContext.Provider value={{ email, setEmail }}>
       {children}
     </ReceitasContext.Provider>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import ReceitasContext from '../context/ReceitasContext';
 
 function Login() {
+  const { email, setEmail } = useContext(ReceitasContext);
+  const [enable, setEnable] = useState(false);
+  const [pass, setPass] = useState('');
+
+  function handleChange() {
+    const six = /.{6,}/;
+    const reg = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
+    setEnable(reg.test(email) && six.test(pass));
+  }
+
+  useEffect(() => {
+    handleChange();
+  }, [email, pass]);
+
   return (
     <section className="login-container">
-      <input data-testid="email-input" type="email" placeholder="Email" />
-      <input data-testid="password-input" type="password" placeholder="Passowrd" />
-      <button data-testid="login-submit-btn" type="button" className="login-btn">
+      <input
+        data-testid="email-input"
+        type="email"
+        placeholder="Email"
+        onChange={({ target }) => setEmail(target.value)}
+      />
+      <input
+        data-testid="password-input"
+        type="password"
+        placeholder="Passowrd"
+        onChange={({ target }) => setPass(target.value)}
+      />
+      <button
+        onClick={() => {}}
+        data-testid="login-submit-btn"
+        type="button"
+        className="login-btn"
+        disabled={!enable}
+      >
         Entrar
       </button>
     </section>
