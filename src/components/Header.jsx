b76b6e627bf4a3a424eb91fabd1c56c91a25359f
William Gomes <williamjog@hotmail.com>
2020-11-20 14:07:23 -0300

    requisito 12 1.0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import propTypes from 'prop-types';
+import ReceitasContext from '../context/ReceitasContext';
 import profile from '../images/profileIcon.svg';
 import search from '../images/searchIcon.svg';
 
 function Header({ title }) {
+  const { hidden, setHidden } = useContext(ReceitasContext);
+
+  const handleHiddenInput = () => (hidden === true ? setHidden(false) : setHidden(true));
+
   return (
     <header className="header-container">
       <title data-testid="page-title" value={ title }>{title}</title>
-      <Link to="/explorar">
+      <Link to="/explorar" onClick={ handleHiddenInput } data-testid="search-input">
         <img data-testid="search-top-btn" src={ search } alt="" />
       </Link>
       <Link to="/perfil">
         <img data-testid="profile-top-btn" src={ profile } alt="" />
       </Link>
+      {hidden ? null : <input type="text" />}
     </header>
   );
 }
