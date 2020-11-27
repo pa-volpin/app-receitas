23d7bd8ee4a20024f3eba5dbe42ba7b4fab990fd
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-11-27 18:27:52 -0300

    :sparkles: Receitas Feitas e Favoritas
    Setado todos os data testId's agr precisa do localStorage e das funções de filtro!!!

--- src/pages/ReceitasFavoritas.jsx
+++ src/pages/ReceitasFavoritas.jsx
@@ -1,16 +1,68 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-import profile from '../images/profileIcon.svg';
+import React, { useContext, useEffect, useState } from 'react';
+import Header from '../components/Header';
+import CardFavorite from '../components/CardFavorite';
+import ReceitasContext from '../context/ReceitasContext';
 
 function ReceitasFavoritas() {
+  const { isFetching, setIsFetching, setTitleHeader,
+    setDisabledSearchIcon, setShowSearchBar } = useContext(ReceitasContext);
+  const [favoritas, setFavoritas] = useState([{
+    id: 'id-da-receita',
+    type: 'comidas-ou-bebidas',
+    area: 'area-da-receita-ou-texto-vazio',
+    category: 'categoria-da-receita-ou-texto-vazio',
+    alcoholicOrNot: 'alcoholic-ou-non-alcoholic-ou-texto-vazio',
+    name: 'nome-da-receita',
+    image: 'imagem-da-receita',
+  }]);
+
+  useEffect(() => {
+    setTitleHeader('Receitas Favoritas');
+    setDisabledSearchIcon(false);
+    setShowSearchBar(false);
+    setIsFetching(true);
+    const localStorageFavoritas = localStorage.getItem('favoriteRecipes');
+    console.log(localStorageFavoritas);
+    if (localStorageFavoritas !== null) {
+      const { favoriteRecipes } = JSON.parse(localStorageFavoritas);
+      setFavoritas(...favoriteRecipes);
+    }
+    setIsFetching(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
-    <div>
-      <title data-testid="page-title">Receitas Favoritas</title>
-      <Link to="/perfil">
-        <img data-testid="profile-top-btn" src={ profile } alt="" />
-      </Link>
-    </div>
+    <main className="favoritas-container">
+      <header className="favoritas-header">
+        <Header />
+      </header>
+      <section className="favoritas-filtros">
+        <button data-testid="filter-by-all-btn" type="button">All</button>
+        <button data-testid="filter-by-food-btn" type="button">Food</button>
+        <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
+      </section>
+      <section className="favoritas-cards">
+        {isFetching
+          ? <p>Loading...</p>
+          : (
+            favoritas.map((favorita, idx) => (
+              <CardFavorite
+                key={ idx }
+                imagePath={ favorita.image }
+                itemName={ favorita.name }
+                id={ favorita.id }
+                itemType={ favorita.type }
+                indexId={ idx }
+                cardType="horizontal"
+                origin={ favorita.area }
+                category={ favorita.category }
+                alcoholic={ favorita.alcoholicOrNot }
+                titlePage="Receitas Favoritas"
+                // {tags.map(data-testid={ `${indexId}-${tagName}-horizontal-tag` })}
+              />
+            ))
+          )}
+      </section>
+    </main>
   );
 }
 
