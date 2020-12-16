1a9aa05c57cb4c1aed4a82c83bd8e6ae1f31893d
pa-volpin <paulo.volpin@gmail.com>
2020-12-16 15:43:32 -0300

    Updates

---
+++ precache-manifest.393ffc446ee2d77fe922af94029741d2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe46604607587a1947a50d5401163e4",
+    "url": "/app-receitas/index.html"
+  },
+  {
+    "revision": "1de95d2dccda2e1783e9",
+    "url": "/app-receitas/static/css/main.7275702f.chunk.css"
+  },
+  {
+    "revision": "a8aabd4c3c743c721a69",
+    "url": "/app-receitas/static/js/2.1fb96b02.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/app-receitas/static/js/2.1fb96b02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1de95d2dccda2e1783e9",
+    "url": "/app-receitas/static/js/main.3fdc2550.chunk.js"
+  },
+  {
+    "revision": "f0cd473e09855cc953e0",
+    "url": "/app-receitas/static/js/runtime-main.0f6d492e.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/app-receitas/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "c48f87a199c2b3fdbe2bda36a4b15f4e",
+    "url": "/app-receitas/static/media/chef.c48f87a1.png"
+  },
+  {
+    "revision": "d0fdc0c743825bc29585383c5cc34551",
+    "url": "/app-receitas/static/media/cook.d0fdc0c7.jpg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/app-receitas/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/app-receitas/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "4ebd569d8416313159a34ae9e82300aa",
+    "url": "/app-receitas/static/media/fried.4ebd569d.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/app-receitas/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/app-receitas/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "f57dcb82aca89308ddc9f876538bf9b6",
+    "url": "/app-receitas/static/media/recipe.f57dcb82.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/app-receitas/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/app-receitas/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/app-receitas/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
